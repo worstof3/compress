8b2b55ba8732e6cc4a14e4ea38414d8261f0b7d3
worstof3 <33602077+worstof3@users.noreply.github.com>
2018-01-23 21:30:28 +0100

    Huffman tree.

--- tree.py
+++ tree.py
@@ -1,5 +1,5 @@
 """
-Module defines class for representing Huffman tree.
+Module defines class representing Huffman tree.
 
 Classes:
 
@@ -10,8 +10,9 @@ HuffmanTree -- Huffman tree.
 
 from heapq import heapify, heappop, heappush
 from functools import total_ordering
-from itertools import chain
 from collections import OrderedDict
+from bitoperations import int_to_bits
+from itertools import chain
 
 
 @total_ordering
@@ -21,11 +22,12 @@ class Node:
 
     Special methods:
     __init__() -- Initialize attributes.
-    __lt__() -- Frequencies comparison.
-    __eq__() -- Frequencies comparison.
+    __lt__() -- (Frequency, byte) comparison.
+    __eq__() -- (Frequency, byte) comparison.
 
     Attributes:
-    byte -- Byte associated with node, only in leaves it's not empty.
+    byte -- Byte(s) associated with node, for leaf it's one byte for inner
+            node it's left.byte + right.byte.
     freq -- Frequency of a byte.
     left -- Left son of a node.
     right -- Right son of a node.
@@ -45,12 +47,12 @@ class Node:
         self.right = None
 
     def __lt__(self, other):
-        """Frequency comparison."""
-        return self.freq < other.freq
+        """(Frequency, byte) comparison."""
+        return (self.freq, self.byte) < (other.freq, other.byte)
 
     def __eq__(self, other):
-        """Frequency comparison."""
-        return self.freq == other.freq
+        """(Frequency, byte) comparison."""
+        return (self.freq, self.byte) == (other.freq, other.byte)
 
 
 class HuffmanTree:
@@ -58,10 +60,19 @@ class HuffmanTree:
     Class representing Huffman tree.
 
     Methods:
+    __traverse() -- Traverse the tree, calculate length of encoding and codes.
     serialize() -- Create binary representation of a tree.
 
     Special methods:
     __init__() -- Create Huffman tree from given byte frequencies.
+
+    Attributes:
+    leaves_num -- Number of leaves in the tree.
+    root -- Root of the tree.
+    encoding_length -- Length of file encoding.
+    bytes_codes -- Mapping bytes in file on their codes (binary strings).
+    header -- Binary representation of tree (bytes + structure).
+    Last three attributes are calculated when calling serialize().
     """
 
     def __init__(self, frequencies):
@@ -73,75 +84,80 @@ class HuffmanTree:
         create a node with priority equal to sum of priorities of the two
         elements. The created node has the two elements as its children.
         We do this until there is only one node, which is a root of a tree.
+        To ensure that order of bytes written in file header is always the
+        same, each we associate with each node concatenated bytes of its
+        children.
+
         Args:
         frequencies -- Dictionary mapping bytes -> frequencies.
         """
         heap = [Node(byte, freq) for (byte, freq) in frequencies.items()]
         heapify(heap)
-        for _ in range(len(heap) - 1):
+        self.leaves_num = len(heap)
+        for _ in range(self.leaves_num - 1):
             left = heappop(heap)
             right = heappop(heap)
-            new_node = Node(b'', left.freq + right.freq)
+            new_node = Node(left.byte + right.byte, left.freq + right.freq)
             new_node.left = left
             new_node.right = right
             heappush(heap, new_node)
         self.root = heap[0]
+        self.encoding_length = 0
+        self.bytes_codes = {}
+        self.header = []
+
+    def __traverse(self, node, code):
+        """
+        Traverse the tree and create it's structure representation.
+
+        We create tree structure representation in the following way.
+        We start from the tree root and write 0, then we recursively
+        traverse left subtree and write 1, next we write 0 and traverse
+        right subtree and write 1 at the end. In other words, we write 0,
+        if we are going down in the tree, and 1 if we are going up.
+        In addition during traversing tree we are calculating code for
+        each byte and total encoding length.
+
+        Args:
+        node -- Node in the tree.
+        code -- Code created so far.
+        """
+        if node.left is None:
+            self.bytes_codes[node.byte] = code
+            self.header.append(int_to_bits(node.byte, 8))
+            self.encoding_length += node.freq * len(code)
+        else:
+            self.tree_bits.append('0')
+            self.__traverse(node.left, code + '0')
+            self.tree_bits.extend(('1', '0'))
+            self.__traverse(node.right, code + '1')
+            self.tree_bits.append('1')
 
     def serialize(self):
         """
         Create binary representation of a Huffman tree.
 
-        During traversing the tree we build codes and store them in
-        OrderedDict when we reach a leaf.
         Binary representation of a tree is in following form:
         First byte is number of distinct bytes in file minus one.
         It's followed by these bytes and align, to ensure whole
-        file length in bits in divisible by 8.
-        Last part of the header is a tree structure. We start from
-        the tree root and traverse it starting to the left and if we
-        reach a leaf we are going back up. Each edge of the tree is
-        traversed twice. If we are going down we write 0, if we
-        are going up we write 1.
+        file length in bits in divisible by 8. After that is binary
+        representation of tree structure.
 
         Returns:
         header - Binary representation of the tree.
         """
-        self.bytes_codes = OrderedDict()
-        tree_bits = []
-        encoding_length = 0
+        # Writing number of bytes - 1, so we can fit 256, 0 bytes won't be compressed.
+        self.tree_bits = []
+        self.header.append(int_to_bits(self.leaves_num - 1, 8))
 
         # Creating tree structure representation.
-        def traverse(node, code):
-            """
-            Traverse the tree and create it's structure representation.
+        self.__traverse(self.root, '')
 
-            Args:
-            node -- Node in the tree.
-            code -- Code created so far
-            """
-            nonlocal self, tree_bits, encoding_length
-            if node.left is None:
-                self.bytes_codes[node.byte] = code
-                encoding_length += node.freq * len(code)
-                tree_bits.append('1')
-            else:
-                tree_bits.append('0')
-                traverse(node.left, code + '0')
-                tree_bits.append('0')
-                traverse(node.right, code + '1')
-                tree_bits.append('1')
-        traverse(self.root, '')
-
-        # Creating other pieces of tree representation and joining together.
-        # Writing number of bytes - 1, so we can fit 256, 0 bytes won't be compressed.
-        header = [bin(len(self.bytes_codes) - 1)[2:].zfill(8)]
-        # Writing bytes used in file.
-        for byte in self.bytes_codes:
-            header.append(bin(byte)[2:].zfill(8))
         # Align, so total number of bits is divisible by 8.
-        align_length = (8 - (len(tree_bits) - 1 + encoding_length) % 8) % 8
+        align_length = (8 - (len(self.tree_bits) + self.encoding_length) % 8) % 8
         align = align_length * '1'
-        # We use tree_bits without the last one, because it's redundant.
-        header = ''.join(chain(header, align, tree_bits[:-1]))
 
-        return header
+        # Joining all pieces.
+        self.header = ''.join(chain(self.header, (align,), self.tree_bits))
+        del self.tree_bits
+        return self.header
